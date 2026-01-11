97d7b6e92590c6f604e45fd8be5755fb558d660c
Karesse DSouza <karessekend1821@gmail.com>
2026-01-11 22:33:55 +0530

    vite dependency added

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
 
 
@@ -7,7 +7,7 @@ import path from "path";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 8080,
+    port: 8081,
   },
   plugins: [react(), mode === "development"].filter(Boolean),
   resolve: {
